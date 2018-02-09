6cc02296d72097060051979a3e152d7c8736c46a
amistele <andrew@mistele.com>
2018-02-09 16:46:47 -0500

    Renamed files for git because named version numbers are irrelevant. In range_test.py : removed all print statements previously used for debugging. Code functions as expected. Not written in function notation. Lists return all contents of distributed range values, not extremes. Will try to implement that feature in next version. TLDR: removed in-code debugging features, returns list of full ranges

--- range_test.py
+++ range_test.py
@@ -1,4 +1,3 @@
-import math
 
 r_open = int(input("open range?\n"))
 r_close = int(input("close range?\n"))+1
@@ -10,6 +9,8 @@ length = len(pre_range)
 each_length = int(length/n_dist)
 final_length = int(each_length+(length%n_dist))
 
+#for debugging
+"""
 print("size of range=")
 print(str(length))
 
@@ -18,32 +19,22 @@ print(str(each_length))
 
 print("length of final")
 print(str(final_length))
-
-print("")
+"""
+#creates distributions lists withing list_of_dists
 list_of_dists = []
 for i  in range(1,n_dist+1):
-    print(str(i))
     list_of_dists.append([])
-"""
-x = 1
-for item_list in list_of_dists:
-    item_list.append(x)
-    print(item_list)
-    x+=1
-"""
-print(list_of_dists)
-#print(str(pre_range))
 
+#calculates all distributions to lists except final
 initial = r_open
 for i in range(0,n_dist-1):
-    print("item index")
-    print(str(i))
     for x in range(initial, initial+each_length):
         list_of_dists[i].extend([x])
-    print(list_of_dists[i])
     initial += each_length
 
+#calculates final distribution of range / n_dist + remainder
 for x in range(initial, initial+final_length):
     list_of_dists[n_dist-1].extend([x])
-print(list_of_dists[n_dist-1])
-#print(list_of_dists[n_dist-1])
+
+print("")
+print(list_of_dists)
